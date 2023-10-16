247de4a4f526c7484718f22b730920329603c5d2
hofor27 <hofor27@gmail.com>
2023-10-16 11:50:42 -0300

    Change languages

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+// Load the default language (English)
+let currentLanguage = 'en';
 
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("active");
-  navLinks.classList.toggle("responsive");
-});
+const languageData = {
+  en: {
+    headerTitle: 'Horst',
+    projectsLink: 'Projects',
+    skillsLink: 'Skills',
+    contactLink: 'Contact',
+    title:"I'm a Frontend Developer"
+  },
+  pt: {
+    headerTitle: 'Horst',
+    projectsLink: 'Projetos',
+    skillsLink: 'Competências',
+    contactLink: 'Contato',
+    title:"Sou Frontend Developer"
+  },
+  de: {
+    headerTitle: 'Horst',
+    projectsLink: 'Projekte',
+    skillsLink: 'Fähigkeiten',
+    contactLink: 'Kontakt',
+    title:"Ich bin ein Frontend Developer"
+  },
+};
 
-// Smooth scrolling
-const navItems = document.querySelectorAll(".nav-links a");
+function updateLanguageUI() {
+  const languageBtn = document.getElementById('languageBtn');
+  const headerTitle = document.getElementById('header-title');
+  const projectsLink = document.getElementById('projects-link');
+  const skillsLink = document.getElementById('skills-link');
+  const contactLink = document.getElementById('contact-link');
 
-navItems.forEach(function (navItem) {
-  navItem.addEventListener("click", function (event) {
-    event.preventDefault();
+  languageBtn.innerHTML = `${languageData[currentLanguage].language} <i class="fa fa-caret-down"></i>`;
+  headerTitle.innerText = languageData[currentLanguage].headerTitle;
+  projectsLink.innerText = languageData[currentLanguage].projectsLink;
+  skillsLink.innerText = languageData[currentLanguage].skillsLink;
+  contactLink.innerText = languageData[currentLanguage].contactLink;
+}
 
-    const target = document.querySelector(navItem.getAttribute("href"));
-    const offsetTop = target.offsetTop;
+function changeLanguage(language) {
+  currentLanguage = language;
+  updateLanguageUI();
+}
 
-    window.scrollTo({
-      top: offsetTop,
-      behavior: "smooth",
-    });
-  });
-});
+// Initialize the UI with the default language
+updateLanguageUI();
